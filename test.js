aec3c3ae34ccdd223d27d51e0ebbb484f2c61798
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2015-12-03 17:52:09 +0100

    Add type checking on a plane ✈️

--- test.js
+++ test.js
@@ -4,27 +4,31 @@ var eslintRules = require('./');
 
 describe('Eslint rules', function () {
 
-  it('should be defined', function () {
-    assert(eslintRules);
-  });
+    it('should be defined', function () {
+        assert(eslintRules);
+    });
 
-  it('should contain some rules', function () {
-    assert.notEqual(Object.keys(eslintRules).length, 0);
-  });
+    it('should contain some rules', function () {
+        assert.notEqual(Object.keys(eslintRules).length, 0);
+    });
 
-  it('should have a description property', function () {
-    assert(eslintRules[0].hasOwnProperty('description'));
-  });
+    it('should have a description property', function () {
+        assert(eslintRules[0].hasOwnProperty('description'));
+        assert(typeof eslintRules[0].description === 'string');
+    });
 
-  it('should have a link property', function () {
-    assert(eslintRules[0].hasOwnProperty('link'));
-  });
+    it('should have a link property', function () {
+        assert(eslintRules[0].hasOwnProperty('link'));
+        assert(typeof eslintRules[0].link === 'string');
+    });
 
-  it('should have a recommended property', function () {
-    assert(eslintRules[0].hasOwnProperty('recommended'));
-  });
+    it('should have a recommended property', function () {
+        assert(eslintRules[0].hasOwnProperty('recommended'));
+        assert(typeof eslintRules[0].recommended === 'boolean');
+    });
 
-  it('should have a type property', function () {
-    assert(eslintRules[0].hasOwnProperty('type'));
-  });
+    it('should have a type property', function () {
+        assert(eslintRules[0].hasOwnProperty('type'));
+        assert(typeof eslintRules[0].type === 'string');
+    });
 });
