220e60323abe9f83ada598a9b87a51e60718009c
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2016-02-14 00:27:44 +0100

    Bump deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+0.0.2 2016.02.14
+Update Travis node versions
+
 0.0.1 2015.10.24
 Add first rule `comma-dangle`
--- test.js
+++ test.js
@@ -3,32 +3,31 @@ var assert = require('assert');
 var eslintRules = require('./');
 
 describe('Eslint rules', function () {
+  it('should be defined', function () {
+    assert(eslintRules);
+  });
 
-    it('should be defined', function () {
-        assert(eslintRules);
-    });
+  it('should contain some rules', function () {
+    assert.notEqual(Object.keys(eslintRules).length, 0);
+  });
 
-    it('should contain some rules', function () {
-        assert.notEqual(Object.keys(eslintRules).length, 0);
-    });
+  it('should have a description property', function () {
+    assert(eslintRules[0].hasOwnProperty('description'));
+    assert(typeof eslintRules[0].description === 'string');
+  });
 
-    it('should have a description property', function () {
-        assert(eslintRules[0].hasOwnProperty('description'));
-        assert(typeof eslintRules[0].description === 'string');
-    });
+  it('should have a link property', function () {
+    assert(eslintRules[0].hasOwnProperty('link'));
+    assert(typeof eslintRules[0].link === 'string');
+  });
 
-    it('should have a link property', function () {
-        assert(eslintRules[0].hasOwnProperty('link'));
-        assert(typeof eslintRules[0].link === 'string');
-    });
+  it('should have a recommended property', function () {
+    assert(eslintRules[0].hasOwnProperty('recommended'));
+    assert(typeof eslintRules[0].recommended === 'boolean');
+  });
 
-    it('should have a recommended property', function () {
-        assert(eslintRules[0].hasOwnProperty('recommended'));
-        assert(typeof eslintRules[0].recommended === 'boolean');
-    });
-
-    it('should have a type property', function () {
-        assert(eslintRules[0].hasOwnProperty('type'));
-        assert(typeof eslintRules[0].type === 'string');
-    });
+  it('should have a type property', function () {
+    assert(eslintRules[0].hasOwnProperty('type'));
+    assert(typeof eslintRules[0].type === 'string');
+  });
 });
